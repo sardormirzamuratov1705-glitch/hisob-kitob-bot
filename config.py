e62acf5edb793e8a8d1f564a693f856665b57731
sardormirzamuratov1705-glitch <sardormirzamuratov1705@gmail.com>
2026-07-18 07:34:50 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -25,3 +25,41 @@ WEBHOOK_HOST = os.getenv("WEBHOOK_HOST", "").rstrip("/")
 WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}" if WEBHOOK_HOST else ""
 PORT = int(os.getenv("PORT", "8080"))
+
+# ---------- OBUNA TARIFLARI VA TO'LOV REKVIZITLARI (6-BOSQICH) ----------
+# DIQQAT: quyidagi narxlar va rekvizitlar VAQTINCHALIK QIYMATLAR - haqiqiy
+# ishga tushirishdan oldin .env orqali (yoki shu yerda to'g'ridan-to'g'ri)
+# o'zingizning narxlaringiz/karta-Click-Payme raqamlaringizga almashtiring!
+SUBSCRIPTION_PRICE_1_MONTH = int(os.getenv("SUBSCRIPTION_PRICE_1_MONTH", "50000"))
+SUBSCRIPTION_PRICE_3_MONTHS = int(os.getenv("SUBSCRIPTION_PRICE_3_MONTHS", "135000"))
+SUBSCRIPTION_PRICE_12_MONTHS = int(os.getenv("SUBSCRIPTION_PRICE_12_MONTHS", "480000"))
+
+PAYMENT_CARD_NUMBER = os.getenv("PAYMENT_CARD_NUMBER", "8600 1234 5678 9012")
+PAYMENT_CARD_HOLDER = os.getenv("PAYMENT_CARD_HOLDER", "F.I.Sh. To'ldiring")
+PAYMENT_CLICK_NUMBER = os.getenv("PAYMENT_CLICK_NUMBER", "+998 90 123 45 67")
+PAYMENT_PAYME_NUMBER = os.getenv("PAYMENT_PAYME_NUMBER", "+998 90 123 45 67")
+
+# Tariflar ro'yxati - handlers/subscription.py shu yerdan o'qiydi (tugmalar,
+# narx ko'rsatish va tanlangan tarifga qarab subscription_until'ni necha
+# kunga uzaytirish kerakligini aniqlash uchun). "days" - 7-bosqichda
+# to'lov tasdiqlanganda owners.subscription_until shunga qarab uzaytiriladi.
+SUBSCRIPTION_PLANS = {
+    "1m": {
+        "label": "1 oy",
+        "days": 30,
+        "price": SUBSCRIPTION_PRICE_1_MONTH,
+        "discount_note": None,
+    },
+    "3m": {
+        "label": "3 oy",
+        "days": 90,
+        "price": SUBSCRIPTION_PRICE_3_MONTHS,
+        "discount_note": "chegirmali",
+    },
+    "12m": {
+        "label": "12 oy",
+        "days": 365,
+        "price": SUBSCRIPTION_PRICE_12_MONTHS,
+        "discount_note": "chegirmali",
+    },
+}
